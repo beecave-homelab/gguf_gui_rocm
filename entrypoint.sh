697781a19c5a5c9f2596cb921fcf24d133bc0681
elvee <lowie@beecave.nl>
2024-12-17 21:46:20 +0100

    Automated commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-export PYENV_ROOT="$HOME/.pyenv"
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-. .venv/bin/activate
-cd /app
-exec "$@" 
+
+# export PYENV_ROOT="$HOME/.pyenv"
+# command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+# eval "$(pyenv init -)"
+# . .venv/bin/activate
+
+## Use this to run the app with entrypoint.sh in the Dockerfile
+
+# COPY entrypoint.sh /app/entrypoint.sh
+# RUN chmod +x /app/entrypoint.sh
+# ENTRYPOINT [ "/app/entrypoint.sh" ]
+
+streamlit run main.py --server.address 0.0.0.0
